05e0c3f56ad757ee3fc1925ae4b65d3ee0c3569f
BeZvas <alekskr2003@Gmail.com>
2024-07-30 22:03:31 +0400

    файлы для докер

---
+++ docker/client/main.py
@@ -0,0 +1,36 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import sqlite3
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+tt = ''
+ 
+async def callback_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await context.bot.send_message(chat_id=chat_id, text=tt)
+    name = update.effective_chat.full_name
+    context.job_queue.run_repeating(start, interval=3, data=name, chat_id=chat_id) # first = 3, send "SettSetting a timer" after 3 sec
+
+async def start(context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=context.job.chat_id, text=f'hello world {context.job.data}!')
+
+if __name__ == '__main__':
+    connection = sqlite3.connect('db/test.db')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT * FROM states''')
+    tts = cursor.fetchall()
+    tt = tts[0]
+    connection.close()
+
+    application = ApplicationBuilder().token('7422363040:AAHOxVTgX0j1k54YNIv-NYJpPGxmp0qXStM').build()
+    
+    timer_handler = CommandHandler('timer', callback_timer)
+    
+    application.add_handler(timer_handler)
+    
+    application.run_polling()
